fe2fb56f644e2e0cec2365c8d98b97f8fd9e57a2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-09 05:57:18 +0530

    Update send_emails.php

--- send_emails.php
+++ send_emails.php
@@ -1,7 +1,22 @@
 <?php 
+    //who you need to send the email
     $receiver = "jehankandysl.gmail.com";
+
+    //subject of the email
     $subject = "Email Send Via XAMPP";
+    
+    //body of the email
     $body = "JehanKandy email sending";
+    
+    //your email
+    /*
+        ---<<--- IMPORTANT --->>---
+
+        $sender email must be same that you change in C:\xampp\sendmail configaration file
+
+        otherwise email never send
+
+    */
     $sender = "jehankandy@gmail.com";
 
     if(mail($receiver, $subject, $body, $sender)){
